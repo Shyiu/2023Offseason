57e26e15ff2db31c81002f23c29bee1b2c7826eb
VR1922 <varunrk22@gmail.com>
2023-08-15 09:59:50 -0400

    Made AemMotorTesting.java
    Made to test the position of the arm.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/ArmMotorTesting.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.debug;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.RobotConstants;
+import org.firstinspires.ftc.teamcode.Subsystems.Delivery;
+
+@TeleOp(name="Arm Debug", group="Test Code")
+public class ArmMotorTesting extends LinearOpMode {
+    DcMotor arm;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        arm = hardwareMap.get(DcMotor.class, RobotConstants.arm);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            arm.setPower(-(gamepad1.right_stick_y)/16.0);
+
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Arm Position", arm.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
